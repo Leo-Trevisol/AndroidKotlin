30962405ac01095187a5ef563630beb8f22118d4
leo-trevisol <leotrevisol.2021@gmail.com>
2024-10-28 23:58:33 -0300

    Algumas coisas

--- Workspaces/Workspaces curso Udemy/EstudandoKotlinUdemy/app/src/main/java/com/project/estudandokotlinudemy/Main.kt
+++ Workspaces/Workspaces curso Udemy/EstudandoKotlinUdemy/app/src/main/java/com/project/estudandokotlinudemy/Main.kt
@@ -1,5 +1,11 @@
 package com.project.estudandokotlinudemy
 
+import java.lang.Double.max
+import java.lang.Double.min
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.math.round
+
 fun main() {
     println("Teste")
 
@@ -61,6 +67,139 @@ fun main() {
 
     println("Nome é " + nome3 + " e sobrenome é " + sobrenome)
 
+    println(helloWorld(nome))
 
+    helloWorld()
+
+    println("Tamanho do nome é de ${nome.length}")
+
+    println("Primeira letra do nome é ${nome[0]}")
+
+    println("Nome começa com l ? ${nome.startsWith("l")}")
+
+    println("Nome termina com l ? ${nome.endsWith("l")}")
+
+    nome = "leonardo"
+
+    println("Meio do nome = ${nome.substring(4)} ")
+
+    println("Maiscula ${nome.uppercase()}")
+
+    println(" Minuscula ${nome.lowercase()}")
+
+    println("O maior entre 5 e 10 é ${max(5,10)}")
+    println("O menor entre 5 e 10 é ${min(5,10)}")
+    println("Arredondando 15,55 = ${round(15.55f)}")
+
+    var n = 51
+
+    if(n >= 1 && n <= 50){
+
+    }
+
+    //Simplificando condição
+
+    if(n in 1..50){
+
+    }
+
+    when(n){
+        1 -> println("Numero = 1")
+        1 -> println("Numero = 1")
+        1 -> println("Numero = 1")
+        1 -> println("Numero = 1")
+        else -> println("Numero desconhecido")
+    }
+
+    if (1 == 2){
+        print("Informe um valor: ")
+        var s = readLine()
+
+        if(s != null && !s.isEmpty()){
+            println("Informou ${s}")
+        }
+    }
+
+    //FOR
+
+    for(i in 1..10)
+        print("$i ")
+
+    val str = "Leonardo"
+    for(char in str)
+        print("$char ")
+
+    for(i in 1..10 step 2)
+        print("$i ")
+
+    for(i in 20 downTo 0)
+        print("$i ")
+
+    for(i in 30 downTo 0 step 3)
+        print("$i ")
+
+    //ACEITAR VARIAVEL NULA
+
+    var s : String? = null
+
+    println(s?.length)
+
+    //ASSUME QUE PODE SER NULA
+    println(s!!.length)
+
+    try {
+        var i : Int = 10/0;
+    }catch (e : Exception){
+        println("Erro generico!")
+    }
+
+    //OPERADOR ELVIS
+
+    val str2 :String? = null
+    println(str ?: "null")
+
+    //LET - VAI EXECUTAR O BLOCO QUANDO FOR != NULL
+
+    str2?.let {
+        it.length
+        it.lowercase()
+        //...
+    }
 
 }
+
+fun helloWorld(nome : String) : String{
+    return "Hello $nome!"
+}
+
+fun helloWorld() : Unit{
+    println("Hello World!")
+}
+
+fun oneLine() = println("One line fun")
+
+fun sum(value : Int) : Int {
+    return value + value
+}
+fun sumOneLine(value : Int) : Int =  value + value
+
+fun saudacao(dia : Boolean) : String{
+    return if(dia){
+        "Bom dia"
+    }else{
+        "Boa noite"
+    }
+}
+    fun saudacaoSimp(dia : Boolean)  = if(dia){
+            "Bom dia"
+        }else{
+            "Boa noite"
+        }
+
+    fun bonusWhen(vlr : Int) : String{
+        return when(vlr){
+            2000 -> "2000"
+            else -> "Valor fora"
+        }
+    }
+
