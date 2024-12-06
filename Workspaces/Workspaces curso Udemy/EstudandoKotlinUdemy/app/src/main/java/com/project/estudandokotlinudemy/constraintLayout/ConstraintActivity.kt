17073373d9a47837a98ad82323491a0fbacfe465
leo-trevisol <leotrevisol.2021@gmail.com>
2024-12-06 19:29:22 -0300

    ConstraintLayout - circle e chain

---
+++ Workspaces/Workspaces curso Udemy/EstudandoKotlinUdemy/app/src/main/java/com/project/estudandokotlinudemy/constraintLayout/ConstraintActivity.kt
@@ -0,0 +1,12 @@
+package com.project.estudandokotlinudemy.constraintLayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.project.estudandokotlinudemy.R
+
+class ConstraintActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint)
+    }
+}
