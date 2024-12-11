e545aa8e2f6a30502f79a47770cda469853c27b1
leo-trevisol <leotrevisol.2021@gmail.com>
2024-12-10 22:52:15 -0300

    Novo projeto de cadastro de convidados com banco

---
+++ Workspaces/Workspaces curso Udemy/ProjetoMotivation/app/src/main/java/com/project/projetomotivation/SecurityPreferences.java
@@ -0,0 +1,9 @@
+package com.project.projetomotivation;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SecurityPreferences(context : Context) {
+
+    private val security ; SharedPreferences = context.
+}
