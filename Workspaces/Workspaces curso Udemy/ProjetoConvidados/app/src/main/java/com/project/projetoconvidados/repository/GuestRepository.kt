6f0d1bfe877914eb9d38b63ff1494f57454b28d0
leo-trevisol <leotrevisol.2021@gmail.com>
2024-12-15 11:30:48 -0300

    Adicionando método de update, delete e select

--- Workspaces/Workspaces curso Udemy/ProjetoConvidados/app/src/main/java/com/project/projetoconvidados/repository/GuestRepository.kt
+++ Workspaces/Workspaces curso Udemy/ProjetoConvidados/app/src/main/java/com/project/projetoconvidados/repository/GuestRepository.kt
@@ -1,5 +1,6 @@
 package com.project.projetoconvidados.repository
 
+import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.content.Context
 import com.project.projetoconvidados.constants.DataBaseConstants
@@ -42,7 +43,80 @@ class GuestRepository private constructor(context : Context){
         }
     }
 
-    fun update(){
+    fun update(guest : GuestModel): Boolean {
 
+        return try {
+            //pega a conexão weitable, dizendo que vamos inserir
+            val db = guestDataBase.writableDatabase
+
+            val presence = if(guest.presence) 1 else 0
+
+            //Lista de valores com chave e valor
+            val values = ContentValues()
+            values.put(DataBaseConstants.COLUMS.NAME, guest.name)
+            values.put(DataBaseConstants.COLUMS.PRESENCE, presence)
+
+            val selection = DataBaseConstants.GUEST.TABLE_NAME + " = ?"
+            val args = arrayOf(guest.id.toString())
+
+            db.update(DataBaseConstants.GUEST.TABLE_NAME, values,selection, args)
+            true
+        }catch (e : Exception){
+            false
+        }
     }
+
+    fun delete(id : Int): Boolean {
+
+        return try {
+            //pega a conexão weitable, dizendo que vamos inserir
+            val db = guestDataBase.writableDatabase
+
+            val selection = DataBaseConstants.GUEST.TABLE_NAME + " = ?"
+            val args = arrayOf(id.toString())
+
+            db.delete(DataBaseConstants.GUEST.TABLE_NAME, selection, args)
+            true
+        }catch (e : Exception){
+            false
+        }
+    }
+
+    @SuppressLint("Range")
+    fun getAll() : List<GuestModel>{
+
+        val list = mutableListOf<GuestModel>()
+
+        try {
+
+            val db = guestDataBase.readableDatabase
+
+            val selection = arrayOf(
+                DataBaseConstants.COLUMS.ID,
+                DataBaseConstants.COLUMS.NAME,
+                DataBaseConstants.COLUMS.PRESENCE
+            )
+
+            val cursor = db.query(
+                DataBaseConstants.GUEST.TABLE_NAME, selection,
+                null, null, null, null, null
+            )
+
+            if(cursor != null && cursor.count > 0){
+                while (cursor.moveToNext()){
+                    val id = cursor.getInt(cursor.getColumnIndex(DataBaseConstants.COLUMS.ID))
+                    val name = cursor.getString(cursor.getColumnIndex(DataBaseConstants.COLUMS.NAME))
+                    val presence = cursor.getInt(cursor.getColumnIndex(DataBaseConstants.COLUMS.PRESENCE))
+
+                    list.add(GuestModel(id, name, presence == 1))
+                }
+            }
+
+        }catch (e : Exception){
+            return list
+        }
+
+        return list
+    }
+
 }
