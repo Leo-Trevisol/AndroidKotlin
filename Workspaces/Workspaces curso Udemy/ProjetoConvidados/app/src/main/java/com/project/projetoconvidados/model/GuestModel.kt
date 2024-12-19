575ece45da0318473378d448ff5d7cfbf7b6696b
leo-trevisol <leotrevisol.2021@gmail.com>
2024-12-18 22:09:34 -0300

    Implementando ROOM e kapt

--- Workspaces/Workspaces curso Udemy/ProjetoConvidados/app/src/main/java/com/project/projetoconvidados/model/GuestModel.kt
+++ Workspaces/Workspaces curso Udemy/ProjetoConvidados/app/src/main/java/com/project/projetoconvidados/model/GuestModel.kt
@@ -1,4 +1,20 @@
 package com.project.projetoconvidados.model
 
-class GuestModel(val id : Int, val name : String, val presence : Boolean) {
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Guest")
+class GuestModel{
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id : Int = 0
+
+    @ColumnInfo(name = "name")
+    val name : String = ""
+
+    @ColumnInfo(name = "presence")
+    val presence : Boolean = false
+
 }
