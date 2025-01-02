762515ebfe7e6795c9ef4ff228224daeeadc9db1
leo-trevisol <leotrevisol.2021@gmail.com>
2025-01-02 19:06:53 -0300

    Salvando dados de person no banco

--- Workspaces/Workspaces curso Udemy/ProjetoTasks/app/src/main/java/com/devmasterteam/tasks/viewmodel/LoginViewModel.kt
+++ Workspaces/Workspaces curso Udemy/ProjetoTasks/app/src/main/java/com/devmasterteam/tasks/viewmodel/LoginViewModel.kt
@@ -1,17 +1,22 @@
 package com.devmasterteam.tasks.viewmodel
 
 import android.app.Application
+import androidx.constraintlayout.widget.ConstraintAttribute
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.devmasterteam.tasks.service.constants.TaskConstants
 import com.devmasterteam.tasks.service.listener.APIListener
 import com.devmasterteam.tasks.service.model.PersonModel
 import com.devmasterteam.tasks.service.model.ValidationModel
 import com.devmasterteam.tasks.service.repository.PersonRepository
+import com.devmasterteam.tasks.service.repository.SecurityPreferences
 
 class LoginViewModel(application: Application) : AndroidViewModel(application) {
 
     private val personRepository = PersonRepository(application.applicationContext)
+
+    private val securityPreferences = SecurityPreferences(application.applicationContext)
 
     private val _login = MutableLiveData<ValidationModel>()
     val login : LiveData<ValidationModel> = _login
@@ -23,6 +28,9 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
         personRepository.login(email, password, object : APIListener<PersonModel>{
 
             override fun onSucess(response: PersonModel) {
+                securityPreferences.store(TaskConstants.SHARED.TOKEN_KEY, response.token)
+                securityPreferences.store(TaskConstants.SHARED.PERSON_KEY, response.personKey)
+                securityPreferences.store(TaskConstants.SHARED.PERSON_NAME, response.name)
                 _login.value = ValidationModel()
             }
 
